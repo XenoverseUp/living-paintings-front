87d089b3cc0f766b041b021290023ce2951262c2
XenoverseUp <xenoverseup@gmail.com>
2025-05-26 16:31:56 +0300

    fix: deployment

--- src/components/xr-card.tsx
+++ src/components/xr-card.tsx
@@ -9,7 +9,7 @@ type Props = {
   subtitle: string;
 } & Partial<HTMLDivElement>;
 
-export default function XRCard({ className, id, subtitle, title }: Props) {
+export default function XRCard({ className, subtitle, title }: Props) {
   return (
     <figure className={cn(className, "relative flex flex-col")}>
       <div className="absolute top-4 left-4 flex items-center justify-center space-x-2 rounded-full bg-black/20 px-3 py-1.5 text-white shadow-lg backdrop-blur-xl select-none">
